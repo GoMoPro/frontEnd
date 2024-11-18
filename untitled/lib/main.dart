18fed022c28d9f08c7f5a8841d3429a784f9222c
eyeofsol <sjh991101@gmail.com>
2024-11-18 11:31:30 +0900

    홈 스크린 추가
    캘린더와 일일 일정을 분리해서 짜기 위해 홈 스크린을 추가

--- untitled/lib/main.dart
+++ untitled/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:untitled/screen/calendar_screen.dart';
+import 'package:untitled/screen/home_screen.dart';
 import 'firebase_options.dart'; // Firebase 설정 파일을 불러옵니다.
 import 'screen/login_screen.dart'; // 로그인 화면을 불러옵니다.
 import 'package:intl/date_symbol_data_local.dart'; // locale 정보를 불러옴.
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: const Color(0xFF1976D2), // 앱의 기본 색상을 파란색으로 설정
       ),
-      home: const CalendarScreen(), // 앱 시작 시 로그인 화면을 표시
+      home: const HomeScreen(), // 앱 시작 시 로그인 화면을 표시
     );
   }
 }
---
+++ untitled/lib/screen/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget{
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Text('Nano Calendar'),
+    );
+  }
+}
